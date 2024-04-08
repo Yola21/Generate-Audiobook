b51a88483d9fb742e326f578eaab57750cda9e47
Yola21 <yashkhorja4@gmail.com>
2024-04-08 20:13:35 -0300

    fix

--- Backend/index.mjs
+++ Backend/index.mjs
@@ -1,8 +1,19 @@
 import AWS from "aws-sdk";
 
-const S3_BUCKET_NAME = "cloud-term-assignment-s3";
-const SNS_TOPIC_NAME = "Upload-Resume";
+const S3_BUCKET_NAME = "resume-parser-s3";
+const SNS_TOPIC_NAME = "ResumeParserSNS";
+const AWS_REGION = "us-east-1";
+const AWS_ACCESS_KEY_ID = "ASIATCKANRQAOQDP32FS";
+const AWS_SECRET_ACCESS_KEY = "Hs6aLu+ThZJ1Xj5Pq7ylmHkD+4wOowfKkWN05OcP";
+const AWS_SESSION_TOKEN =
+  "IQoJb3JpZ2luX2VjEM7//////////wEaCXVzLXdlc3QtMiJHMEUCIQDwX5AIEJEL7valrASSr5GaiaQoGY/H2wLt4s46tlqjJQIgXZpImR1rPYyd77LlG6UeXgvZ8t8KIMbRH9XzqC297hkqsgII9///////////ARAAGgwyMTExMjUzNzM5NTIiDGTx89B4/3wRXrQy1CqGAujp4IWuWT8J6flMsreg6MCZ0zJDU+eZPTH+BL0zJaq/GDVRzZgyybTOk4OyHgkoTEx4C8lj7bbSC+Y8IWb0E6Sm5QejojI3iaTtrXwwox0YmoH6OPrEi1WZGC1cV3SyDz6qoGIMwT6BByzi+l21xQ/BzRkxWzYG3PumgSYi4xxqJuDcdnFCIO8lWgMGQmyb5V1v62heig0t9uwb6dYgK8u5hAPRHT50m6qlrw76A623KBWh134tlTDmZj7EnESIT+qKgeT9hZhdQuvrSL2gJF7ccFaHDXD9kOAWAy5kzwX1dy9xVEmbKgtxNSR0bOOkD/lXvCMz6+z8Ugz6gYBjqCLS12QvPxowqsvRsAY6nQGFcsLqSaeH2oooEM10RJuTg/ToT0PBIR8Lea4fmSzCeq5XPONBfVmVJIqcUP/cZBPa6yrbxyGB/8z/Qp1J32y1jgN5sn6OT1NlcuikECL5YHm4AgHonpsxCS8OS/JvbMRxksie3Av8tV7g6MWyihWvoR9Ka5X2Dd89xSd6k8PXTwJ51+blRRx9oaIi2Uazbj9b/912f3kqaLCDjfqo";
 
+AWS.config.update({
+  region: AWS_REGION,
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  sessionToken: AWS_SESSION_TOKEN,
+});
 const s3 = new AWS.S3();
 const textract = new AWS.Textract();
 const sns = new AWS.SNS();
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,18 +4,20 @@ import Home from "./HomePage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+const AWS_REGION = "us-east-1";
+const AWS_ACCESS_KEY_ID = "ASIATCKANRQAOQDP32FS";
+const AWS_SECRET_ACCESS_KEY = "Hs6aLu+ThZJ1Xj5Pq7ylmHkD+4wOowfKkWN05OcP";
+const AWS_SESSION_TOKEN =
+  "IQoJb3JpZ2luX2VjEM7//////////wEaCXVzLXdlc3QtMiJHMEUCIQDwX5AIEJEL7valrASSr5GaiaQoGY/H2wLt4s46tlqjJQIgXZpImR1rPYyd77LlG6UeXgvZ8t8KIMbRH9XzqC297hkqsgII9///////////ARAAGgwyMTExMjUzNzM5NTIiDGTx89B4/3wRXrQy1CqGAujp4IWuWT8J6flMsreg6MCZ0zJDU+eZPTH+BL0zJaq/GDVRzZgyybTOk4OyHgkoTEx4C8lj7bbSC+Y8IWb0E6Sm5QejojI3iaTtrXwwox0YmoH6OPrEi1WZGC1cV3SyDz6qoGIMwT6BByzi+l21xQ/BzRkxWzYG3PumgSYi4xxqJuDcdnFCIO8lWgMGQmyb5V1v62heig0t9uwb6dYgK8u5hAPRHT50m6qlrw76A623KBWh134tlTDmZj7EnESIT+qKgeT9hZhdQuvrSL2gJF7ccFaHDXD9kOAWAy5kzwX1dy9xVEmbKgtxNSR0bOOkD/lXvCMz6+z8Ugz6gYBjqCLS12QvPxowqsvRsAY6nQGFcsLqSaeH2oooEM10RJuTg/ToT0PBIR8Lea4fmSzCeq5XPONBfVmVJIqcUP/cZBPa6yrbxyGB/8z/Qp1J32y1jgN5sn6OT1NlcuikECL5YHm4AgHonpsxCS8OS/JvbMRxksie3Av8tV7g6MWyihWvoR9Ka5X2Dd89xSd6k8PXTwJ51+blRRx9oaIi2Uazbj9b/912f3kqaLCDjfqo";
+
 function App() {
   useEffect(() => {
-    const configureAWS = async () =>
-      await AWS.config.update({
-        accessKeyId: "ASIATCKANRQAN24XLHU3",
-        secretAccessKey: "09fdxXFZyWzYrru0vwgUL43wuXsfsGAGjhDvD95q",
-        region: "us-east-1",
-        sessionToken:
-          "IQoJb3JpZ2luX2VjEMr//////////wEaCXVzLXdlc3QtMiJHMEUCIQCQozSWHH797XBhbyVQDHh+fbtoya8H1SjlfyZaynke8AIgJOaAPEUFujBwsiPGwNtsapHU+60gVtOczvwUhgJY70oqsgII8///////////ARAAGgwyMTExMjUzNzM5NTIiDA23M7y2uNtyX6E3SyqGAvVbTy2xGPtpK374m+jms7jHwtwNkz3TX9T5JH2vLcG1P98phIMdzkOLijn9sba8QRZ2RjhcgdsZ+o9+C+JTOug9FXhNhnpDAW4+16/AtZxdxyAC1RSyUgK1SyAghwzWZocMs96Sy4+w/i8h8fbE9hHjfuHWRm5toR1gdIPErVmSbe5JfUFU3pGJYktf59aUwi0NKM7q3dHu58+sNAMUBbWiQQzPAdoMVyLfphpb9Gsg4VjJ8D7yKm/P3uqoCpQjViFdGHjjdbfOhLim7hsU4hFxuspse/SryzITPZlX+U1Rst/i9zEsOxa0s99/wFOzxiPdWkusydYcCb5vtEETDiU8PzHuGJow79zQsAY6nQG1zzZS3mcZL5uSZq5jvukW5C4MfHRZuTJr0Lt+oQLsfjFS8EviMQ4ocrPKi8fi3u7W9TIReJDaSrCkZNMfAo9eIGKBlyulcVh6VFQLvZ/8NHXMN1yBq1BFPNFJl0lISv0969mbCTGnXwTQECkWhOv1e41YyyExtQbkQtBg0GpccuiIaN+vd1Mq2pRaHN/ft3H9dSKENt9aqmFOXLEj",
-      });
-
-    configureAWS();
+    AWS.config.update({
+      region: AWS_REGION,
+      accessKeyId: AWS_ACCESS_KEY_ID,
+      secretAccessKey: AWS_SECRET_ACCESS_KEY,
+      sessionToken: AWS_SESSION_TOKEN,
+    });
   }, []);
 
   return (
