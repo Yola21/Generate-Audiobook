0668046b05d24608747bd8ed7c672e70f0864ae3
Yola21 <yashkhorja4@gmail.com>
2024-04-09 03:47:54 -0300

    fix

--- Frontend/src/HomePage.js
+++ Frontend/src/HomePage.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Button, Grid, TextField, Typography } from "@material-ui/core";
 import { invokeLambdaFunction } from "./invokeLambda";
 
-const API = "https://wu78yrzcoh.execute-api.us-east-1.amazonaws.com/prod";
+const API = "https://t86h0hvi91.execute-api.us-east-1.amazonaws.com/prod";
 
 function Home() {
   const [resume, setResume] = useState(null);
